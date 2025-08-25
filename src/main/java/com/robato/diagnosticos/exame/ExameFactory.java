6d23104ee2966533dfc5f64641c80f19032e9adc
Cassia Gomes <128179609+cassiaagomes@users.noreply.github.com>
2025-08-25 17:17:48 -0300

    fix

--- src/main/java/com/robato/diagnosticos/exame/ExameFactory.java
+++ src/main/java/com/robato/diagnosticos/exame/ExameFactory.java
@@ -8,16 +8,12 @@ public class ExameFactory {
             case RESSONANCIA -> new RessonanciaMagnetica();
             case HEMOGRAMA_COMPLETO -> new HemogramaCompleto();
             case GLICOSE -> new Glicose();
-            case SOROLOGIA -> new Sorologia();
             case UREIA -> new Ureia();
             case CREATININA -> new Creatinina();
             case HDL -> new HDL();
             case LDL -> new LDL();
-            case TRIGLICERIDEOS -> new Triglicerideos();
             case TGO_AST -> new TgoAst();
             case TGP_ALT -> new TgpAlt();
-            case SODIO -> new Sodio();
-            case POTASSIO -> new Potassio();
             case HEMOGRAMA -> new Hemograma();
             
             default -> throw new IllegalArgumentException("Tipo de exame desconhecido ou não implementado na fábrica: " + tipo);
